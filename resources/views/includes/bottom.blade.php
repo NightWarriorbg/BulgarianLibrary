3ae0591ff863f7cf7c4244da21ae28b509507595
Radoslav Enev <nightWarrior.bg@gmail.com>
2021-08-13 23:21:03 +0300

    Adding jquery to bottom(temp fix)

--- resources/views/includes/bottom.blade.php
+++ resources/views/includes/bottom.blade.php
@@ -4,4 +4,5 @@
 </button>
 
 <!-- Adding site javascript -->
+<script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
