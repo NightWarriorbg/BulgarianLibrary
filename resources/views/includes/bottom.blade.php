cfe2b74c51dc57115bd7d922c20c2a9957697ad8
Radoslav Enev <nightWarrior.bg@gmail.com>
2021-08-13 17:12:10 +0300

    Adding/updating jquery, bootstrap, vue via npm

--- resources/views/includes/bottom.blade.php
+++ resources/views/includes/bottom.blade.php
@@ -3,9 +3,5 @@
     <i class="bi bi-chevron-up"></i>
 </button>
 
-<!-- jQuery-->
-<script src="{{ asset('js/jQuery-3-6-0.js') }}"></script>
-<!-- Javascript|Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 <!-- Adding site javascript -->
 <script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,7 +7,8 @@
 <title>{{ config('app.name') }} - @yield('title')</title>
 
 <!-- Styles -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 <link href="{{ asset('css/auth.css') }}" rel="stylesheet">
 <link rel="icon" type="image/png" href="{{ asset('img/icons/book-icon.png') }}">
